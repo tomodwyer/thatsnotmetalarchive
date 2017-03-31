886fe9cb52ca50139878d838804b459a5b31e145
Tom O'Dwyer <24863179+tomodwyer@users.noreply.github.com>
2017-03-31 20:38:33 +0100

    Setup default task and run sequence

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-const gulp = require('gulp');
+const del = require('del');
 const exec = require('child_process').execSync;
+const gulp = require('gulp');
 const gutil = require('gulp-util');
 const path = require('path');
-const del = require('del');
+const runSequence = require('run-sequence');
 
 const paths = {
   dist: path.join(process.cwd(), '_site'),
@@ -14,14 +15,21 @@ function buildJekyll() {
 
   const cmd = `bundle exec jekyll build --source ${paths.src} --destination ${paths.dist}`;
   const output = exec(cmd, {encoding: 'utf-8'});
-  gutil.log(`Jekyll: ${output}`);
+  return gutil.log(`Jekyll: ${output}`);
 }
 
-gulp.task('clear', () => {
+gulp.task('clean', () => {
   del.sync(paths.dist);
-  gutil.log(`Deleted: ${paths.dist}`);
+  return gutil.log(`Deleted: ${paths.dist}`);
 });
 
 gulp.task('jekyll', () => {
   buildJekyll();
+});
+
+gulp.task('default', () => {
+  runSequence(
+    'clean',
+    'jekyll'
+  )
 });
