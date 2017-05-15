0b1d3577eba82bc3c2a703d3f6332faea21a7b3d
Tom O'Dwyer <24863179+tomodwyer@users.noreply.github.com>
2017-05-15 20:19:59 +0100

    Disable ESLint on the main application.js file

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,6 +1,10 @@
+/* eslint-disable */
+
 //=require jquery/dist/jquery.min.js
 //=require jquery.scrolly.min.js
 //=require jquery.scrollex/jquery.scrollex.min.js
 //=require skel-framework/dist/skel.min.js
 //=require util.js
 //=require main.js
+
+/* eslint-enable */
